42431dd53bdea81e81df6b57438a49f7ca88f767
Jordan Hickerson <hickersonjordan@gmail.com>
2021-07-06 19:27:41 -0500

    tab1 of dashboard working with firefox, need to add cryptos

--- ag_dash/get_roi_px.py
+++ ag_dash/get_roi_px.py
@@ -4,8 +4,10 @@ import plotly.express as px
 def get_roi_plot(id):
     symbol = id.split("_")[0].upper()
     combined_df = pd.read_csv("combined_roi.csv",parse_dates=True, index_col="timestamp")
-    combined_df.index = pd.to_datetime(grad_roi_df.index)
+    combined_df.index = pd.to_datetime(combined_df.index)
     symbol_df = combined_df[[f"LIN_{symbol}",f"GRAD_{symbol}",f"IMB_{symbol}",f"XGB_{symbol}"]]
-    fig = px.line(symbol_df, x="timestamp", y = [f"LIN_{symbol}",f"GRAD_{symbol}",f"IMB_{symbol}",f"XGB_{symbol}"])
+    #fig = px.line(symbol_df, x="timestamp", y = [f"LIN_{symbol}",f"GRAD_{symbol}",f"IMB_{symbol}",f"XGB_{symbol}"])
+    fig = px.line(symbol_df)
+    
     return fig
 
